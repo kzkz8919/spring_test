c5cf52398200a0c7e9dcd254ae042a466b9e1a6a
kzkz8919 <kzkz8919@126.com>
2017-11-02 21:00:02 +0800

    <c:forEach >标签

--- src/main/java/com/jj/controller/demo/DemoController.java
+++ src/main/java/com/jj/controller/demo/DemoController.java
@@ -1,5 +1,7 @@
 package com.jj.controller.demo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class DemoController {
 
 	private final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
 	
+	/*	
 	@RequestMapping(value = "/demo", method = RequestMethod.GET)
 	public String demo(Map<String, Object> map){
 		logger.info("this is demo.");
@@ -22,8 +26,27 @@ public class DemoController {
 		//return "/WEB-INF/jsp/test.jsp";
 		return "test";
 	}
+	 */	
 	
-	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String demo(Map<String, Object> map){
+
+		List<List<String>> list = new ArrayList<List<String>>();
+		List<String> info1 = new ArrayList<String>();
+		info1.add("a1");
+		info1.add("b1");
+		info1.add("c1");
+		info1.add("d1");
+		List<String> info2 = new ArrayList<String>();
+		info2.add("a2");
+		info2.add("b2");
+		info2.add("c2");
+		info2.add("d2");
+		list.add(info1);
+		list.add(info2);
+		map.put("list", list);
+		return "list";
+	}
 	
 	
 }
