f03e034155c167b08dea7266dcee51ed69057c0e
kzkz8919 <kzkz8919@126.com>
2017-11-17 14:58:15 +0800

    拦截器练习，有问题

---
+++ src/main/java/com/jj/controller/login/LoginController.java
@@ -0,0 +1,28 @@
+package com.jj.controller.login;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class LoginController {
+	
+	private final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String login(Map<String, Object> map){
+		logger.info("this is login.");
+		return "login";
+	}
+
+	@RequestMapping(value = "login", method = RequestMethod.GET)
+	public String login(String username, String password){
+		logger.info("this is welcome.");
+		return "welcome";
+	}
+}
